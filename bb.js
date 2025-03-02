0dbf7ff2da2657656dcbeef66f4b374a28130ed5
gaurav5886 <gauravchawla5886@gmail.com>
2025-03-02 20:13:46 +0530

    Add files via upload

---
+++ bb.js
@@ -0,0 +1,10 @@
+function createCar(make, model, year) {
+    return {
+        make,
+        model,
+        year,
+        describeCar() {
+            console.log(`This car is a ${this.year} ${this.make} ${this.model}.`);
+        }
+    };
+}
